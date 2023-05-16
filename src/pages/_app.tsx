536dc87572762dcf2c0d2248caf36a579c863875
Michael Ries <michael.riess@mailfence.com>
2023-05-15 20:24:34 -0400

    feat(): add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,35 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+const Header = () => (
+  <header
+    style={{ display: "flex", justifyContent: "space-between", padding: 20 }}
+  >
+    <h1>My App</h1>
+    <SignedIn>
+      {/* Mount the UserButton component */}
+      <UserButton />
+    </SignedIn>
+    <SignedOut>
+      {/* Signed out users get sign in button */}
+      <SignInButton />
+    </SignedOut>
+  </header>
+);
+
+const MyApp: AppType = ({ Component, pageProps }) => (
+  <ClerkProvider {...pageProps}>
+    <Header />
+    <Component {...pageProps} />
+  </ClerkProvider>
+);
 
 export default api.withTRPC(MyApp);
