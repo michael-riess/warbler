3989c594e2004adaaaf30a3cb9740b4a7e0b6639
Michael Ries <michael.riess@mailfence.com>
2023-05-17 04:26:34 -0400

    feat(nav): add side-nav and improve responsiveness

--- src/components/page-template.tsx
+++ src/components/page-template.tsx
@@ -3,8 +3,8 @@ import { type PropsWithChildren } from 'react';
 export const PageTemplate = (props: PropsWithChildren) => {
     return (
         // 64px is the height of the header
-        <main className="flex justify-center overscroll-none h-[calc(100vh-64px)]">
-            <div className="w-full rounded-md border-x border-yellow-200 md:max-w-2xl overflow-y-scroll">
+        <main className="flex justify-center overscroll-none h-[calc(100vh-64px)] sm:h-screen">
+            <div className="w-full rounded-md border-x border-yellow-200 md:max-w-xl overflow-y-scroll">
                 {props.children}
             </div>
         </main>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,16 @@ import {
 } from '@clerk/nextjs';
 import { type AppType } from 'next/app';
 import Head from 'next/head';
+import Link from 'next/link';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import '~/styles/globals.css';
 import { api } from '~/utils/api';
 
-const Header = () => (
+const HeaderNav = () => (
     <header className="flex justify-center">
-        <div className="h-16 flex w-full items-center justify-between md:max-w-2xl">
+        <div className="h-16 flex w-full items-center justify-between md:max-w-xl">
             <h1>Warbler</h1>
             <SignedIn>
                 {/* Mount the UserButton component */}
@@ -28,6 +29,34 @@ const Header = () => (
         </div>
     </header>
 );
+
+const SideNav = () => {
+    return (
+        <header>
+            <div className="h-16 flex flex-col w-full items-center justify-between md:max-w-xl mt-4">
+                <SignedIn>
+                    <UserButton
+                        appearance={{
+                            elements: {
+                                // Hide circle avatar & replace with text button
+                                userButtonAvatarBox: 'hidden',
+                                userButtonTrigger:
+                                    'before:content-["Settings"] focus:shadow-none',
+                            },
+                        }}
+                    />
+                    <Link href={`/`}>
+                        <span>Explore</span>
+                    </Link>
+                </SignedIn>
+                <SignedOut>
+                    {/* Signed out users get sign in button */}
+                    <SignInButton />
+                </SignedOut>
+            </div>
+        </header>
+    );
+};
 
 const MyApp: AppType = ({ Component, pageProps }) => (
     <ClerkProvider {...pageProps}>
@@ -48,8 +77,20 @@ const MyApp: AppType = ({ Component, pageProps }) => (
             pauseOnHover
             theme="light"
         />
-        <Header />
-        <Component {...pageProps} />
+        <div className="block sm:hidden">
+            <HeaderNav />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-7 lg:grid-cols-12">
+            <div className="hidden sm:block col-start-1 lg:col-start-2">
+                <SideNav />
+            </div>
+
+            <div className="col-span-1 sm:col-span-5 lg:col-span-8">
+                <Component {...pageProps} />
+            </div>
+
+            <div className="">{/* TODO: Add search & widgets */}</div>
+        </div>
     </ClerkProvider>
 );
 
